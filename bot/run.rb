75fd0139e0ffda2da4e3a421d32711c0b57aaad0
Chris Schneider <chris@christopher-schneider.com>
2008-08-03 21:32:47 -0600

    Rip out more crap from run.rb

--- bot/run.rb
+++ bot/run.rb
@@ -2,24 +2,7 @@
 
 require 'rubygems'
 require 'logger_bot'
-require 'getopt/long'
 
-# This code just launches our logger with certain parameters.
-#
-# If options are specified, they override the default yaml file.  If a
-# different yaml file is specified, its settings are read, then overridden
-# by options.
-#
-# Passwords hash cannot be specified on the command-line - yaml or nothing.
-
-opt = Getopt::Long.getopts(
-  ['--silent',      '-s', Getopt::BOOLEAN],
-  ['--loud',        '-l', Getopt::BOOLEAN],
-  ['--network',     '-n', Getopt::OPTIONAL],
-  ['--output-dir',  '-o', Getopt::OPTIONAL],
-  ['--master',      '-m', Getopt::OPTIONAL],
-  ['--yaml',        '-y', Getopt::OPTIONAL]
-)
 
 opt['yaml'] ||= File.dirname(__FILE__) + '/default.yml'
 if File.exists?(opt['yaml'])
