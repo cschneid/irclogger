b5635904c18fd87a6542814da3e9f582cae5a6f7
Chris Schneider <chris@christopher-schneider.com>
2009-03-14 22:47:33 -0600

    Load the gist js

--- public/actions.js
+++ public/actions.js
@@ -1,13 +1,15 @@
 
 $(document).ready(function() {
   $("#links a[href*=gist.github.com]").click(function() {
+    $("body").append("<div id='dialog'></div>");
     var url = $(this).attr("href");
     // Launch lightbox
-    $("#dialog").html(url);
-    $("#dialog").dialog();
+    $("#dialog").html("<script src='" + url + ".js'></script>");
+    $("#dialog").dialog({
+    	title: url,
+	close: function(ev, ui) { $(this).remove(); } 
+    });
     return false;
-
-    return true;
   });
 })
 
